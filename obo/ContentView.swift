cf3607615cc32647ecc3c1243749caf885056451
bill donner <billdonner@gmail.com>
2026-02-08 08:53:13 -0600

    Initial Commit

---
+++ obo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  obo
+//
+//  Created by bill donner on 2/8/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ obo/oboApp.swift
@@ -0,0 +1,17 @@
+//
+//  oboApp.swift
+//  obo
+//
+//  Created by bill donner on 2/8/26.
+//
+
+import SwiftUI
+
+@main
+struct oboApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
